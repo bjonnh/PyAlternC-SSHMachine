1f1d2669d8e7dd91dde0de04d69a657487c89d63
Jonathan BISSON <bissonjonathan@gmail.com>
2011-06-22 14:43:39 +0200

    Added the verbose argument Added a way to put user and password in script (interactive or non-interactive mode) Added configuration strings Cleaning up the way messages are sent. The server name is now sent to standard output alone (only if verbose is off). Added login failure check

--- pyalternc-sshmachine.py
+++ pyalternc-sshmachine.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 # A small script to open machines on an AlternC platform
 
 import mechanize
@@ -7,18 +8,23 @@ import re
 import sys
 import getpass
 
-class AlternC:
-    def __init__(self, username, password):
+INTERACTIVE = False # Set to False to set password,user and server inside the script
+Configuration_server = {'login':"https://admin.lautre.net/admin/", 'vm_start':"https://admin.lautre.net/admin/vm.php?action=start", 'vm_stop':"https://admin.lautre.net/admin/vm.php?action=stop", 'logout':"https://admin.lautre.net/admin/mem_logout.php", 'pattern_machine':".*\>(.*vds\.lautre\.net).*"}
+Configuration_user = {'login':"foo",'password':"bar"}
+
+class Connect_AlternC:
+    def __init__(self, configuration,username, password,verbose):
         self.username = username
         self.password = password
 
         # Adapt these to your platform
-        self.url_login = 'https://admin.lautre.net/admin/'
-        self.url_vm_start = 'https://admin.lautre.net/admin/vm.php?action=start'
-        self.url_vm_stop = 'https://admin.lautre.net/admin/vm.php?action=stop'
-        self.url_logout = 'https://admin.lautre.net/admin/mem_logout.php'
-        self.pattern_machine = re.compile('.*\>(.*vds\.lautre\.net).*')
+        self.url_login = configuration['login']
+        self.url_vm_start = configuration['vm_start']
+        self.url_vm_stop = configuration['vm_stop']
+        self.url_logout = configuration['logout']
+        self.pattern_machine = re.compile(configuration['pattern_machine'])
 
+        self.verbose = verbose
         self.browser = mechanize.Browser(factory=mechanize.RobustFactory())
         cj = cookielib.LWPCookieJar()
         self.browser.set_cookiejar(cj)
@@ -43,43 +49,61 @@ class AlternC:
             self.browser.form['username'] = self.username
             self.browser.form['password'] = self.password
         except Exception as e:
-            print ('The following error occured: \n"%s"' % e)
-            print ('A good idea is to open a browser and see if you can log in from there.')
-            print ('URL:', self.url)
-            raw_input()
-            exit()
+            sys.stderr.write ('Error: \n"%s"' % e)
+            sys.stderr.write ('Check if you can login from URL: %s' % self.url_login)
+            exit(1)
+        if self.verbose:
+            print ("Login to %s" % self.url_login)
         self.browser.submit()
-
+        login_error = re.search("User or password incorrect", self.browser.response().read())
+        if login_error:
+            sys.stderr.write ('Error: Login incorrect')
+            exit(2)
     def logout(self):
+        if self.verbose:
+            print ("Logout")
         self.browser.open(self.url_logout)
 
     def open_machine(self):
         self.login()
-
+        if self.verbose:
+            print ("Opening machine")
         self.browser.open(self.url_vm_start)
+        
         self.page = self.browser.response().read()
-
-        print ("Machine : %s" % self.pattern_machine.findall(self.page))
+        Machine = self.pattern_machine.findall(self.page)
+        if len(Machine)>0:
+            print("%s\n" % Machine[0])
         AC.logout()
 
     def close_machine(self):
         self.login()
+        if self.verbose:
+            print ("Closing machine")
         self.browser.open(self.url_vm_stop)
         AC.logout()
 
-parser = argparse.ArgumentParser(description='Open and close virtual machines on AlternC platforms')
 
-parser.add_argument('-u','--user',dest="user",required=True)
+parser = argparse.ArgumentParser(description='Open and close virtual machines on AlternC platforms')
 parser.add_argument('-m','--mode',help="open or close",choices=['open','close'],dest="mode",required=True)
-parser.add_argument('-p','--password',dest="password")
+parser.add_argument('-v','--verbose',help="be more verbose",dest="verbose",action="store_true")
+if INTERACTIVE == True:
+    parser.add_argument('-u','--user',dest="user",required=True)   
+    parser.add_argument('-p','--password',dest="password")
+else:
+    user = Configuration_user['login']
+    password = Configuration_user['password']
+
 args = parser.parse_args()
 
-if (args.password is None):
-    password = getpass.getpass()
-else:
-    password = args.password
+if INTERACTIVE == True:
+    user = args.user
+    if (args.password is None):
+        password = getpass.getpass()
+    else:
+        password = args.password
 
-AC = AlternC(args.user, password)
+AC = Connect_AlternC(Configuration_server,user, password,verbose=args.verbose)
 if args.mode == "open":
     AC.open_machine()
 elif args.mode == "close":
